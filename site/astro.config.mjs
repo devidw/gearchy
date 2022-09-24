3bf753fed42cceea4a63326b7daf0e8b6da8e6b4
David Wolf <68775926+devidw@users.noreply.github.com>
2022-09-24 17:34:29 +0000

    update pnpm-lock.yaml, astro.config.mjs and package.json

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
 import svelte from '@astrojs/svelte'
 import UnoCss from 'unocss/astro'
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), image(), UnoCss()],
-  output: 'server',
-  adapter: vercel(),
 })
